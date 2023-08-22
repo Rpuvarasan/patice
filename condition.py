3dc029bad6ce111f29ebf56742763cc3a8bc420b
Rpuvarasan <141003534+Rpuvarasan@users.noreply.github.com>
2023-08-22 09:26:09 +0530

    Add files via upload

---
+++ condition.py
@@ -0,0 +1,56 @@
+##age=int(input('Enter your age'))
+##if(age>=60):
+##        print('your senior citizen')
+##else:
+##    print('your are not a senior citizen')
+
+##task 2
+
+##num=int(input("enter your value"))
+##if(num%2==0):
+##    print('odd number')
+##else:
+##    print('even number')
+
+##task 3
+##num=int(input("enter your value"))
+##if(num>=0):
+##    print('positive')
+##else:
+##    print('negative')
+
+## task 4
+##a=int(input("enter your slaery"))
+##b=int(input("how may your working in this company?"))
+##c=a/5
+##if(b>=5):
+##    print('5% bones for you:',c)
+##else:
+##    print('your not eligible')
+
+####task5
+##a=input('enter the length')
+##b=input('enter the breath')
+##if(a==b):
+##    print('its is square')
+##else:
+##    print('its is not square')
+
+##task 6
+
+##a=int(input('enter total number of school days'))
+##b=int(input('how may day student prestent'))
+##c=b/a*100
+##if(c>=75):
+##    print("your are eligible to attend the exam")
+##else:
+##    print("your not eligable to attend the exam")
+
+##task 7
+num=int(input('enter your number'))
+if(num%5==0):
+    print('Hello')
+else:
+    print('Bye')
+    
+
---
+++ elif.py
@@ -0,0 +1,123 @@
+##
+####task2
+##mark=int(input('enter student mark'))
+##if(mark>39):
+##    if(mark>90)and(mark<=100):
+##        print('A grade')
+##    elif(mark>80) and (mark<=90):
+##        print('B grade')
+##    elif(mark>=60) and (mark<=80):
+##        print('C grade')
+##    elif(mark<59)and (mark>=40):
+##        print("D grade")
+##else:
+##    print('ur grade is F (fail)')
+##
+##task 1
+##
+##unit=int(input('enter ur electric bill unit'))
+##if(unit>100):
+##    if(unit>100)and (unit<=200):
+##         amt=(unit-100)*5
+##         print("ur electric bill rs",amt)
+##    elif(unit>200):
+##        amt2=(unit-100)*10
+##        print('ur electric bill rs',amt2)
+##else:
+##    print('u have no chages')
+
+
+##task3
+##rate=int(input('Enter ur bike price'))
+##if(rate<=50000):
+##    t1=(rate*100)/(100+5)
+##    print("ur rode tax amount is:",t1)
+##elif(rate>50000)and(rate<=100000):
+##    t2=(rate*100)/(100+10)
+##    print('ur rode tax amount is:',t2)
+##elif(rate>100000):
+##    t3=(rate*100)/(100+15)
+##    print('ur rode tax amount is:',t3)
+
+
+####task 4
+##num=int(input('enter ur number'))
+##if(num<8):
+##    if(num==1):
+##        print('monday')
+##    elif(num==2):
+##        print("tuesday")
+##    elif(num==3):
+##        print("wednesday")
+##    elif(num==4):
+##        print("thursday")
+##    elif(num==5):
+##        print("friday")
+##    elif(num==6):
+##        print("saturday")
+##    elif(num==7):
+##        print("sunday")
+##else:
+##    print("enter the valued number")
+
+##task 5
+
+##num=int(input('enter ur number'))
+##if(num<13):
+##    if(num==1):
+##        print('1 for JAN month and number of days 31')
+##    elif(num==2):
+##        print("2 for FEB month and number of days 28")
+##    elif(num==3):
+##        print("3 for MAR month and number of days 31")
+##    elif(num==4):
+##        print("4 for APR month and number of days 30")
+##    elif(num==5):
+##        print("5 for MAY month and number of days 31")
+##    elif(num==6):
+##        print("6 for JUN month and number of days 30")
+##    elif(num==7):
+##        print("7 for JUL month and number of days 31")
+##    elif(num==8):
+##        print("8 for AUG month and number of days 31")
+##    elif(num==9):
+##        print("9 for SEP month and number of days 30")
+##    elif(num==10):
+##        print("31 for OCT month and number of days 31")
+##    elif(num==11):
+##        print("11 for NOV month and number of days 30")
+##    elif(num==12):
+##        print("12 for DEC month and number of days 31")
+##        
+##else:
+##    print("enter the valued number")
+####task6
+##a=float(input('enter the A value in points'))
+##b=float(input('enter the B value in points'))
+##c=float(input('enter the C value in points'))
+##if(a==b)and(a==c):
+##    print('its a equalent triangle')
+##elif(a!=b)and(a!=c):
+##    print('its a scalent triangle')
+##elif(a==b)or(a==c):
+##    print('its a isosceles triangle')
+
+##task7
+##a=float(input('enter the A value in points'))
+##b=float(input('enter the B value in points'))
+##c=float(input('enter the C value in points'))
+##if(a<b)and(b<c):
+##    print('b is the second bigest value')
+##elif(b<a)and(a<c):
+##    print('a is the second bigest value')
+##elif(c<a)and(c>b):
+##    print('c is the second bigest value')
+
+
+
+
+
+
+
+    
+        
